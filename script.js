27eb311d6f93e50738a6321a9f2bfc8ab730aaf8
Rand al'Thor <alexanderhogle@gmail.com>
2019-11-13 23:19:55 -0600

    adds api key & url base variables

---
+++ script.js
@@ -0,0 +1,19 @@
+// SETUP VARIABLES
+//===================================
+
+var authKey = "8a705b565438ec0cd8e8e6babb82ad33";
+
+//Search Parameters
+var queryTerm = "";
+
+//URL Base
+
+var queryURLBase = "http://api.openweathermap.org/data/2.5/weather?appid=" + authKey;
+
+
+// FUNCTIONS
+//===================================
+
+
+// MAIN PROCESSES
+//===================================
