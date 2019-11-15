ff271d5ef2dbe905d0ea0e7969e1ac50532d7dec
Rand al'Thor <alexanderhogle@gmail.com>
2019-11-15 01:23:03 -0600

    adds ajax and click function for todays forecast

--- script.js
+++ script.js
@@ -8,12 +8,62 @@ var queryTerm = "";
 
 //URL Base
 
-var queryURLBase = "http://api.openweathermap.org/data/2.5/weather?appid=" + authKey;
+var queryURLBase = "http://api.openweathermap.org/data/2.5/weather?appid=" + authKey + "&units=imperial";
 
 
 // FUNCTIONS
 //===================================
 
+function runQuery(queryURL){
+
+    //AJAX Function
+    $.ajax({url: queryURL, method: "GET"})
+        .done(function(response) {
+
+            // Start Dumping to HTML Here
+            var cardSection = $('<div>');
+            cardSection.addClass("card-body");
+            $('#card').append(cardSection);
+
+            (cardSection).append( "<h2>" + response.name + "</h2>");
+            (cardSection).append( "<h5>" + "Tempurature: " + response.main.temp + "</h5>");
+            (cardSection).append( "<h5>" + "Humidity: " + response.main.humidity + "</h5>");
+            (cardSection).append( "<h5>" + "Wind Speed: " + response.wind.speed + "</h5>");
+            (cardSection).append( "<h5>" + "UV Index: " + "</h5>");
+
+
+            console.log(response);
+            console.log(response.main.temp);
+        })
+}
+
+
+
 
 // MAIN PROCESSES
 //===================================
+
+$('#searchBtn').on('click', function(event) {
+
+    event.preventDefault();
+
+    // Get Search Term
+    queryTerm = $('#search').val().trim();
+
+    var newURL = queryURLBase + "&q=" + queryTerm;
+
+    console.log(newURL);
+
+    runQuery(newURL);
+
+    return false;
+
+})
+
+
+//1. Retrieve user-input and convert to variables
+//2. Use those variables to run an AJAX call to the OpenWeather API
+//3. Breakdown the OpenWeather Object into useable fields
+//4. Dynamically generate html content
+
+//5. Dealing with "edge cases" -- bugs or situations that are not intuitive
